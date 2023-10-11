213190332c65f4823ae115b29bee320b25474045
mikah13 <anhminhhoang13@gmail.com>
2023-10-10 21:35:30 -0700

    feat: added uuid api call

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from typing import Union
 from PIL import Image
-from fastapi import FastAPI, File, Response, UploadFile
+from fastapi import FastAPI, File, HTTPException, Response, UploadFile
 from io import BytesIO
-from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uuid
 import zipfile
@@ -54,16 +54,6 @@ async def get_supported_extensions():
     return {"extensions": image_formats}
 
 
-@app.get("/metadata")
-async def upload_metadata():
-    if format not in image_formats:
-        return {"message": "Invalid format"}
-    str_uuid = str(uuid.uuid4())
-    message = {"uuid": str_uuid, "format": format}
-    process[str_uuid] = {"uuid": str_uuid, "format": format}
-    return message
-
-
 @app.get("/uuid")
 async def get_uuid():
     str_uuid = str(uuid.uuid4())
@@ -74,8 +64,32 @@ async def get_uuid():
 @app.get("/download/{uuid}")
 async def download(uuid: str):
     if uuid not in process:
-        return {"message": "UUID not found"}
-    return process.get(uuid)
+        raise HTTPException(status_code=404, detail="UUID not found")
+
+    # return process[uuid]
+    # Create a BytesIO object to store the zipped files
+    zip_buffer = BytesIO()
+
+    # Create a ZIP file
+    with zipfile.ZipFile("./image.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
+        for image, format, filename in process[uuid]:
+            try:
+                image_data = Image.open(BytesIO(image))
+                # image_buffer = get_image_buffer(image_data)
+                zipf.writestr(
+                    f"{filename}.{format}",
+                    image_data,
+                    compress_type=zipfile.ZIP_DEFLATED,
+                )
+            except Exception as e:
+                print(f"Error processing file {filename}: {str(e)}")
+    zip_buffer.seek(0)
+    response = StreamingResponse(
+        content=zip_buffer,
+        media_type="application/zip",
+        headers={"Content-Disposition": "attachment; filename=download.zip"},
+    )
+    # return response
 
 
 @app.post("/upload")
@@ -91,10 +105,28 @@ async def upload_file(file: UploadFile, format: str, uuid: str):
     original_image.save(
         converted_image, image_formats[format], optimize=True, quality=95
     )
-    if uuid in process:
-        process[uuid].append(converted_image)
-    else:
-        process[uuid] = [converted_image]
+
+    response = StreamingResponse(converted_image, media_type="image/" + format)
+
+    # save_data = {
+    #     "image": converted_image.getvalue(),
+    #     "format": format,
+    #     "filename": file.filename,
+    # }
+
+    # if uuid in process:
+    #     process[uuid].append(save_data)
+    # else:
+    #     process[uuid] = [save_data]
+
     converted_image.seek(0)
 
-    return StreamingResponse(converted_image, media_type="image/" + format)
+    return response
+
+
+def get_image_buffer(image):
+    img_buffer = BytesIO()
+    image.save(img_buffer, "PNG")
+    img_buffer.seek(0)
+
+    return img_buffer
